661e3b6dcf69a6e9609a2f7d2753186c1b7a49d8
Tav Hafner <tav.hafner@gmail.com>
2018-02-16 16:36:59 -0700

    close, didn't quite finish header conditional rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,9 @@ export default function Header({ page, id, name }) {
                                 <img src={mainLogo} alt="logo" />
                             </Link>
                             <Link to={`/bins/${id[0]}`}>
-                            <h1>{`Shelf ${id[0]}`}</h1>
+                            <h1>
+                                {`Shelf ${id[0]}`}
+                            </h1>
                             </Link>
                             {
                                 name === "edit"
